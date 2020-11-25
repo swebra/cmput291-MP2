238a29bee015b2af4eeaab3cf41aabe159a1afcb
imswebra <eric@imswebra.com>
2020-11-25 14:43:02 -0700

    Update create submission script

--- create-submission.sh
+++ create-submission.sh
@@ -2,4 +2,4 @@
 output_file=prj2code.tgz
 ./gen-report-pdf.sh
 rm -f $output_file
-tar -czf $output_file README.txt Report.pdf requirements.txt
+tar -czf $output_file README.txt Report.pdf requirements.txt timePhase1.sh *.py
