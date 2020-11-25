fb2b60c638100a9f9b9888c9e33b11c14fd70b9c
imswebra <eric@imswebra.com>
2020-11-25 11:25:53 -0700

    Update phase2 cli error checking to be consistent with phase1

--- phase2.py
+++ phase2.py
@@ -5,18 +5,22 @@ from logged_in import logged_in
 from utils import print_user_statistics
 
 if __name__ == '__main__':
-    if (len(argv) <= 1 or len(argv) > 3):
-        print("Database port argument expected and optional UID, received", len(argv) - 1)
+    if (len(argv) <= 1):
+        print("Database port expected as a command-line argument")
+        exit(1)
+
+    if (len(argv) > 3):
+        print("Maximum of two command-line arguments expected (port and UID)",
+              "received", len(argv) - 1)
         exit(1)
 
     try:
         port = int(argv[1])
-    except ValueError as e:
-        print("Invalid port")
+    except ValueError:
+        print("Invalid port number given")
         exit(1)
 
     is_connected = db.connect(port)
-
 
     uid = None
     if (len(argv) > 2) and is_connected:
@@ -28,7 +32,6 @@ if __name__ == '__main__':
             print("No statistics found for uid", argv[2])
 
         print("")
-
 
     while (is_connected):
         logged_in(uid)
